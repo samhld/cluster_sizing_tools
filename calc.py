3815c2cba4ba06846902ccc3479d8466b9419fdb
Sam Dillard <azur1289@gmail.com>
2019-05-23 11:02:54 -0700

    finalized calculation

--- calc.py
+++ calc.py
@@ -1,5 +1,3 @@
-# Option 1: take command line args with `sys.argv[]` (same info as in config above)
-
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -12,9 +10,16 @@ args = parser.parse_args()
 mps = args.mps
 nodes = args.nodes
 rf = args.rf
-
+hitNumber = ((mps * (1 / (nodes/rf))) * rf)
+missNumber = ((mps * ((nodes-rf)/nodes) * (rf+1)))
+totalHits = ((mps * (1 / (nodes/rf))) * rf) + ((mps * ((nodes-rf)/nodes) * (rf+1)))
 def totalInternalWrites(mps,nodes,rf):
-    print(mps * (1 / (nodes / rf)) + (mps * (nodes-rf)/rf))
+    #print(((mps * rf) / (nodes/rf)) + ((mps * (rf+1)) / (((nodes-rf)/rf))))
 
+    print(f"Total hits to nodes per second is:  {totalHits}")
+
+    print(f"When metrics hit, they will write {hitNumber} times.")
+    print(f"When metrics miss, they will write {missNumber} times.")
+    #  print(mps * (1 / (nodes / rf)) + (mps * (nodes-rf)/rf))
 if __name__ == "__main__":
     totalInternalWrites(mps,nodes,rf)
